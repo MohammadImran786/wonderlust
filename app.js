b7161f1c2440fbfc29f39f64ffce192349c5e35c
Mohammad Imran <imrangalaxy39@gamil.com>
2026-01-12 13:20:36 +0530

    Added review model and updated listings UI & logic

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const methodOverride=require("method-override");
 const ejsMate=require("ejs-mate");
 const wrapAsync=require("./utils/wrapAsync");
 const ExpressError=require("./utils/ExpressError.js");
-const {listingSchema}=require("./schema.js")
+const {listingSchema,reviewSchema}=require("./schema.js")
+const Review=require("./models/review.js");
 
 // connect mongodb 
 main()
@@ -46,6 +47,16 @@ const validateListing=(req,res,next)=>{
         }
 };
 
+const validateReview=(req,res,next)=>{
+       let {error}=reviewSchema.validate(req.body);
+        if(error){
+            let errMsg=error.details.map((el)=>el.message).join(",");
+            throw new ExpressError(400,errMsg);
+        }else{
+            next();
+        }
+};
+
 //index.route
 app.get("/listings",wrapAsync( async (req,res)=>{
     const allListings=await Listing.find({});
@@ -61,7 +72,7 @@ app.get("/listings/new",(req,res)=>{
 //show route
 app.get("/listings/:id",wrapAsync(async(req,res)=>{
     let {id}=req.params;
-    const listing=await Listing.findById(id);
+    const listing=await Listing.findById(id).populate("reviews");
     res.render("listings/show.ejs",{listing});
 }));
 
@@ -98,6 +109,29 @@ app.delete("/listings/:id",(async(req,res)=>{
     console.log(deletedListing);
     res.redirect("/listings");
 }));
+
+//POST REVIEW ROUTE
+
+app.post("/listings/:id/review",validateReview,wrapAsync(async(req,res)=>{
+let listing=await Listing.findById(req.params.id);
+let newReview=new Review(req.body.review);
+listing.reviews.push(newReview);
+
+await newReview.save();
+await listing.save();
+
+res.redirect(`/listings/${listing._id}`);
+}));
+
+//DELETE REVIEW ROUTE
+
+app.delete("/listings/:id/reviews/:reviewId", wrapAsync(async(req,res)=>{
+    let {id,reviewId}=req.params;
+    await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
+    await Review.findByIdAndDelete(reviewId);
+
+    res.redirect(`/listings/${id}`);
+}))
 
 
 // app.get("/testListing",async (req,res)=>{
